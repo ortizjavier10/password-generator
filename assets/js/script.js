65c389df6f928b8bd1f79840d97b53a19e608728
JAVI <ortiz.javier10@gmail.com>
2020-06-21 08:38:07 -0400

    password length change

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,7 @@ function generatePassword() {
   var confirmLength = (prompt("How many characters would you like your password to contain? Please choose between 8 and 128 characters"));
 
   // alert answer outside of 8 -128 characters
-  while(confirmLength <= 8 || confirmLength >= 128) {
+  while(confirmLength <= 7 || confirmLength >= 129) {
     alert("Password length must be between 8-128 characters Try again");
     var confirmLength = (prompt("How many characters would you like your password to contain? Please choose between 8 and 128 characters"));
     } 
