f2687505c9e75d2e7b19e5165911352b78a085f5
JAVI <ortiz.javier10@gmail.com>
2020-06-21 08:05:27 -0400

    variable declaration

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,13 @@ var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
