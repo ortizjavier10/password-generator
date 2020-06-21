1b890dc0f22ee9e45c4a04a66eda76c2ed5bcf19
JAVI <ortiz.javier10@gmail.com>
2020-06-21 08:10:03 -0400

    password length prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,43 @@
 // Assignment code here
 
+
+// arrays
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+
+//variables
 var confirmLength = "";
 var confirmSpecialCharacter;
 var confirmNumericCharacter;
 var confirmUpperCase;
 var confirmLowerCase;
+
+
+
+
+
+
+
+
+
+
+
+
+// password length pompt
+function generatePassword() {
+  var confirmLength = (prompt("How many characters would you like your password to contain?"));
+
+  // Loop if answer is outside the parameters 
+  while(confirmLength <= 7 || confirmLength >= 51) {
+    alert("Password length must be between 8-50 characters Try again");
+    var confirmLength = (prompt("How many characters would you like your password to contain?"));
+    } 
+
+    // Repeat back how many charactes the user will have  
+    alert(`Your password will have ${confirmLength} characters`);
 
 
 // Get references to the #generate element
